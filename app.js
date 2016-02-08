65232359a143f69ee70e56b39ad786c1e6154bfa
daniel_albinyana <dani@commonpeoplei.com>
2016-02-08 11:57:07 +0100

    PREPEND, BEFORE, AFTER. Before ho posa al mateix nivell del tag que parlis. Prepend ho posa al començament del tag. After ho posa sota. No hi ha texte. .HTML canvia tot el uqe hi hagi dions del tag pel que li passis. .TEXT fa el mateix però et posa texte a saco paco (és a dir, si has posat <p> et posarà &lt;p&gt;n). .REMOVE desapareix del dom .EMPTY desapareix el cotingut però queden les etiquetes html. no hi ha codi

--- app.js
+++ app.js
@@ -3,5 +3,32 @@ $(function() {
 	title.fadeOut();
 	var textAdded = '<p>will it dissapear?</p>';
 	$('.test').append(textAdded);
-	});
+	
+	// for (i=0; i=50; i++){
+	// 	if (i%2==0) {
+	// 		$( '.cell:even').addClass("active")
+	// 		$( '.cell:odd').addClass("inactive")
+
+	// 	}
+	// 	else {
+	// 		$( '.cell:even').addClass("inactive")
+	// 		$( '.cell:odd').addClass("active")
+	// 	}
+	// }
+	
+	
+	
+	// for (i=0; i < $('.cell').length; i++) {
+	// 	if (i % 2 == 0){
+	// 		$('.cell').addClass('active');
+	// 	} 
+	// 	else
+	// 		$('.cell').addClass('inactive');
+	// }
+	// $('.cell').each(function(cell) {
+	// 	$( this ).
+	// })
+	// function()
+  	
+});
 
